e1644c3c2eb3846dc7386b7cdd5793075fe9897d
Lucas Marcondes <lucas.github30@gmail.com>
2022-04-23 00:04:42 -0300

    modificando seção About

--- script/main.js
+++ script/main.js
@@ -1,18 +1,37 @@
+const cabecalhoPrincipal = document.querySelector('.cabecalhoPrincipal')
+const conteudoPrincipal = document.querySelector('.conteudoPrincipal')
+const rodapePrincipal = document.querySelector('.rodapePrincipal')
+
+function esconderConteudo() {
+    cabecalhoPrincipal.style.display = 'none';
+    conteudoPrincipal.style.display = 'none';
+    rodapePrincipal.style.display = 'none';
+}
+
+function exibirConteudo() {
+    cabecalhoPrincipal.style.display = 'block';
+    conteudoPrincipal.style.display = 'block';
+    rodapePrincipal.style.display = 'block';
+}
+
 const botaoFechar = document.querySelector('#botaoFechar')
 const linkAbout = document.querySelector('#linkAbout')
 const animationMove = document.querySelector('#animationMove')
 const section_ObjetivoDoTrabalho = document.querySelector('.about-objetivoDoTrabalho')
-const cabecalhoPrincipal = document.querySelector('.cabecalhoPrincipal')
 
 linkAbout.addEventListener('click', () => {
-    cabecalhoPrincipal.style.position = 'relative'
+    esconderConteudo()
     section_ObjetivoDoTrabalho.classList.toggle('visible')
     section_ObjetivoDoTrabalho.style.animation = "300ms linear fadeIn";
     animationMove.style.animation = "800ms linear moveIn-section";
 })
 
 botaoFechar.addEventListener('click', () => {
+    exibirConteudo()
     setTimeout("section_ObjetivoDoTrabalho.classList.toggle('visible')", 250)
     section_ObjetivoDoTrabalho.style.animation = "300ms linear fadeOut";
     animationMove.style.animation = "400ms linear moveOut-section";
 })
+
+
+
