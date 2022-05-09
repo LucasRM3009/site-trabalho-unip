f7aaa323aca0d1fd88b8e01da8a40de5987753ad
Lucas Marcondes <lucas.github30@gmail.com>
2022-05-09 01:14:47 -0300

    Site finalizado com sucesso

--- script/main.js
+++ script/main.js
@@ -12,9 +12,7 @@ function exibirConteudo() {
     rodapePrincipal.style.display = 'block'
 }
 
-function mudaTitle(title) {
-    document.title = `${title}`
-}
+function mudaTitle(title) {document.title = `${title}`}
 
 function callbackFunctionButton (pageTitle, ms_exibirConteudo, numberVisible, ms_visibleTimeout, ms_visible, numberAnimationMove, ms_animationMove) {
     mudaTitle(pageTitle)
@@ -25,20 +23,60 @@ function callbackFunctionButton (pageTitle, ms_exibirConteudo, numberVisible, ms
 }
 
 function mudaTema() {
-    theme = localStorage.getItem('theme')
+    const sol = document.querySelector('.fa-sun')
+    const lua = document.querySelector('.fa-moon')
+    theme = localStorage.getItem('theme') || 'light-theme'
+
     if(theme === 'light-theme') {
+        sol.style.animation = '500ms linear fadeIn'
+
         document.body.classList.remove('dark-theme')
         document.body.classList.add('light-theme')
 
-        sol.style.display = 'block';
-        lua.style.display = 'none';
+        sol.style.display = 'block'
+        lua.style.display = 'none'
     } else {
+        lua.style.animation = '500ms linear fadeIn' 
+
         document.body.classList.remove('light-theme')
         document.body.classList.add('dark-theme')
 
-        sol.style.display = 'none';
-        lua.style.display = 'block';
+        lua.style.display = 'block'
+        sol.style.display = 'none'
     }
+}
+
+function exibirSecaoObjetivoEDissetacao(numberVisible, ms_visible, numberAnimationMove, ms_animationMove) {
+    visible[numberVisible].classList.toggle('visible')
+    visible[numberVisible].style.animation = `${ms_visible}ms linear fadeIn`
+    animationMove[numberAnimationMove].style.animation = `${ms_animationMove}ms linear moveIn-section`
+}
+
+function exibirSecaoObjetivo() {
+    esconderConteudo()
+    mudaTitle('Inclusão Digital | Objetivo do Trabalho')
+    exibirSecaoObjetivoEDissetacao(1, 300, 0, 800)
+    botaoFechar[0].onclick = () => callbackFunctionButton(title, 200, 1, 250, 300, 0, 400)
+}
+
+function exibirSecaoDissertacao() {
+    esconderConteudo() 
+    mudaTitle('Inclusão Digital | Dissertação')
+    exibirSecaoObjetivoEDissetacao(2, 800, 1, 1500)    
+    botaoFechar[1].onclick = () => callbackFunctionButton(title, 1100, 2, 1150, 1200, 1, 1300)
+}
+
+function toggleMenu(event) {
+    if (event.type === 'touchstart') event.preventDefault()
+    const cabecalhoPrincipalNav = document.querySelector('.cabecalhoPrincipal-nav')
+    const icone = document.querySelector('.fas')
+    cabecalhoPrincipalNav.classList.toggle('active')
+    cabecalhoPrincipal.classList.toggle('cabecalhoPrincipal-bg')
+    icone.classList.toggle('fa-solid')
+    icone.classList.toggle('fa-xmark')
+
+    const active = cabecalhoPrincipalNav.classList.contains('active')
+    event.currentTarget.setAttribute('aria-expanded', active)
 }
 
 const cabecalhoPrincipal = document.querySelector('.cabecalhoPrincipal')
@@ -50,51 +88,26 @@ const linkDissertacao = document.querySelector('#linkDissertacao')
 const botaoFechar = document.querySelectorAll('#botaoFechar')
 const animationMove = document.querySelectorAll('#animationMove')
 const visible = document.querySelectorAll('.visible')
-
 let title = document.title
 
-linkObjetivo.addEventListener('click', () => {
-    esconderConteudo()
-    mudaTitle('Inclusão Digital | Objetivo do Trabalho')
+const mobileMenu = document.querySelector('#mobile-menu')
+const pesquisa = document.querySelector('#pesquisa')
 
-    visible[1].classList.toggle('visible')
-    visible[1].style.animation = '300ms linear fadeIn'
-    animationMove[0].style.animation = '800ms linear moveIn-section'
-
-    botaoFechar[0].onclick = () => callbackFunctionButton(title, 200, 1, 250, 300, 0, 400)
-})
-
-linkDissertacao.addEventListener('click', () => {
-    esconderConteudo() 
-    mudaTitle('Inclusão Digital | Dissertação')
-
-    visible[2].classList.toggle('visible')
-    visible[2].style.animation = '800ms linear fadeIn'
-    animationMove[1].style.animation = '1500ms linear moveIn-section'
-    
-    botaoFechar[1].onclick = () => callbackFunctionButton(title, 1100, 2, 1150, 1200, 1, 1300)
-})
+mobileMenu.addEventListener('click', toggleMenu)
+pesquisa.addEventListener('click', toggleMenu)
+mobileMenu.addEventListener('touchstart', toggleMenu)
+linkObjetivo.addEventListener('click', exibirSecaoObjetivo)
+linkDissertacao.addEventListener('click', exibirSecaoDissertacao)
 
 const switcher = document.querySelector('#iconeMudaCor')
-const sol = document.querySelector('.fa-sun')
-const lua = document.querySelector('.fa-moon')
-
 let theme = localStorage.getItem('theme') || 'light-theme'
 
 switcher.onclick = () => {
-    if (theme === 'light-theme') {
-        document.body.style.animation = '2s linear fadeIn'
-        sol.style.animation = '500ms linear fadeIn'
-        
-        localStorage.setItem('theme', 'dark-theme')
-    } else {
-        document.body.style.animation = '2s linear fadeIn'
-        lua.style.animation = '500ms linear fadeIn' 
-        
-        localStorage.setItem('theme', 'light-theme')
-    }
-    
-    mudaTema() 
-}
+    document.body.style.animation = '2s linear fadeIn'
 
+    if (theme === 'light-theme') { localStorage.setItem('theme', 'dark-theme') } 
+    else { localStorage.setItem('theme', 'light-theme') }
+    
+    mudaTema()
+}
 mudaTema()
